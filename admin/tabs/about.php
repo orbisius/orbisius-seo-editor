1249d3484439106ce557b30dd9361c399b0a9ecb
Slavi Marinov <admin@orbisius.com>
2022-11-29 19:21:58 -0500

    aa

--- admin/tabs/about.php
+++ admin/tabs/about.php
@@ -7,13 +7,14 @@
 
 		<div>
             This plugin was created by the Orbisius team.
-			If you ever run into an error or have a suggestion please contact <a href="https://orbisius.com/support" target="_blank">Orbisius Support</a>
+			If you ever run into an error or have a suggestion please contact
+            <a href="https://orbisius.com/support" target="_blank">Orbisius Support</a>
 		</div>
 
 		<div>
             <h4>Customizations / WordPress Development</h4>
             If you need customization or other WordPress development, migration, hosting set up contact us to get a
-			<a href="https://orbisius.com/free-quote" target="_blank">free quote</a>
+            <a href="<?php echo esc_url("https://orbisius.com/free-quote?utm_source=" . urlencode(ORBISIUS_SEO_EDITOR_PLUGIN_SLUG));?>" target="_blank">free quote</a>
 		</div>
 	</div>
 </div>
